4fea1f9082364f707f06cda452906eada9477d8a
git <shabeerk@notionpress.com>
2021-06-11 12:58:18 +0000

    fibonic

--- ex5_fibonic.php
+++ ex5_fibonic.php
@@ -5,7 +5,7 @@ $n2 = 1;
 echo "<h3>Fibonacci series for first 12 numbers: </h3>";
 echo "\n";
 echo $n1.' '.$n2.' ';
-while ($num < 10; )
+while ($num < 10 )
 {
     $n3 = $n2 + $n1;
     echo $n3.' ';
